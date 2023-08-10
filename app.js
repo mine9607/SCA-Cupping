ac84a488630116d2467b1b39d111d4d540947c38
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-08-09 21:25:33 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import axios from "axios";
 import _ from "lodash";
+
+//create express app framework:
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/form", (req, res) => {
+  res.render("descriptiveform.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
