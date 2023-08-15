dcf0c45f9cfe17e8862befcf3baf2221b0e27c60
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-08-15 06:20:49 -0500

    Added modb schema for coffee and users

--- app.js
+++ app.js
@@ -9,12 +9,40 @@ import _ from "lodash";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Use mongoose to connect to a mongodb with password saved as environment variable
+await mongoose.connect(`mongodb+srv://crunchySumo6960:${process.env.DB_PASSWORD}@cluster0.vwk3y8s.mongodb.net/usersDB`);
+
+//create the mongoose "Schema keyword"
+const Schema = mongoose.Schema;
+
+//create a simple database schema to hold new users (defines the properties of objects which follow this schema)
+const userSchema = new Schema({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String,
+});
+
+const coffeeSchema = new Schema({
+  coffeeId: String,
+  password: String,
+  googleId: String,
+  secret: String,
+});
+
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
 app.get("/form", (req, res) => {
   res.render("descriptiveform.ejs");
+});
+
+app.post("/data", async (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
