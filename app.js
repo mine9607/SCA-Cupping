46f710594adbac474aca25937b5567aa12bf0751
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-08-22 11:04:07 -0500

    added format styling, fixed label values in form, added functionality to register users to the DB  and post form inputs to DB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import axios from "axios";
 import _ from "lodash";
+import { parse } from "path";
 
 //create express app framework:
 const app = express();
@@ -26,44 +27,44 @@ const scoreSchema = new Schema({
   cupperName: String,
   cuppingDate: Date,
   purpose: String,
-  coffeeId: { type: String, default: "123456" },
-  fragranceIntensity: { type: Number, min: 0, max: 15, default: 2 },
+  coffeeId: { type: String, default: "none provided" },
+  fragranceIntensity: { type: Number, min: 0, max: 15, default: 0 },
   fragranceScore: { type: Array, default: 0 },
-  fragranceFinalScore: { type: Number, default: 5 },
-  aromaIntensity: { type: Number, min: 0, max: 15, default: 4 },
+  fragranceFinalScore: { type: Number, default: 0 },
+  aromaIntensity: { type: Number, min: 0, max: 15, default: 0 },
   aromaScore: { type: Array, default: 0 },
-  aromaFinalScore: { type: Number, default: 5 },
-  smellCharacter: { type: Array, default: ["fruity", "berry"] },
-  smellNotes: String,
-  flavorIntensity: { type: Number, min: 0, max: 15, default: 6 },
+  aromaFinalScore: { type: Number, default: 0 },
+  smellCharacter: { type: Array, default: ["none provided"] },
+  smellNotes: { type: String, default: "non provided" },
+  flavorIntensity: { type: Number, min: 0, max: 15, default: 0 },
   flavorScore: { type: Array, default: 0 },
-  flavorFinalScore: { type: Number, default: 5 },
-  aftertasteIntensity: { type: Number, min: 0, max: 15, default: 8 },
+  flavorFinalScore: { type: Number, default: 0 },
+  aftertasteIntensity: { type: Number, min: 0, max: 15, default: 0 },
   aftertasteScore: { type: Array, default: 0 },
-  aftertasteFinalScore: { type: Number, default: 5 },
-  flavorCharacter: { type: Array, default: ["sour", "fermented"] },
-  taste: { type: Array, default: ["salty", "bitter"] },
-  tasteNotes: String,
-  acidIntensity: { type: Number, min: 0, max: 15, default: 10 },
+  aftertasteFinalScore: { type: Number, default: 0 },
+  flavorCharacter: { type: Array, default: ["none provided"] },
+  taste: { type: Array, default: ["none provided"] },
+  tasteNotes: { type: String, default: "non provided" },
+  acidIntensity: { type: Number, min: 0, max: 15, default: 0 },
   acidityScore: { type: Array, default: 0 },
-  acidityFinalScore: { type: Number, default: 5 },
-  acidityCharacter: { type: Array, default: ["sweet", "lemon"] },
-  acidityNotes: String,
-  sweetnessIntensity: { type: Number, min: 0, max: 15, default: 12 },
+  acidityFinalScore: { type: Number, default: 0 },
+  acidityCharacter: { type: Array, default: ["none provided"] },
+  acidityNotes: { type: String, default: "non provided" },
+  sweetnessIntensity: { type: Number, min: 0, max: 15, default: 0 },
   sweetnessScore: { type: Array, default: 0 },
   sweetnessFinalScore: { type: Number, default: 5 },
-  sweetnessNotes: String,
-  mouthfeelIntensity: { type: Number, min: 0, max: 15, default: 14 },
+  sweetnessNotes: { type: String, default: "non provided" },
+  mouthfeelIntensity: { type: Number, min: 0, max: 15, default: 0 },
   mouthfeelScore: { type: Array, default: 0 },
-  mouthfeelFinalScore: { type: Number, default: 5 },
-  mouthfeelCharacter: { type: Array, default: ["silky", "velvety"] },
-  mouthfeelNotes: String,
+  mouthfeelFinalScore: { type: Number, default: 0 },
+  mouthfeelCharacter: { type: Array, default: ["none provided"] },
+  mouthfeelNotes: { type: String, default: "non provided" },
   extrinsicNotes: String,
   overallScore: { type: Array, default: 0 },
-  overallFinalScore: { type: Number, default: 5 },
+  overallFinalScore: { type: Number, default: 0 },
   nonUniformCups: { type: Array, default: 0 },
   defectiveCups: { type: Array, default: 0 },
-  defectType: { type: Array, default: ["moldy", "potato"] },
+  defectType: { type: Array, default: ["none"] },
   totalScore: Number,
 });
 
@@ -78,8 +79,6 @@ const defaultScore = new ScoreModel({});
 const userSchema = new Schema({
   email: { type: String, default: "test@123.com" },
   password: { type: String, default: "123" },
-  googleId: String,
-  secret: String,
 });
 
 //define the UserModel to create the 'users' collection
@@ -110,18 +109,8 @@ const defaultCoffee = new CoffeeModel({});
 //defaultCoffee.save();
 
 //DEFINE RESTFUL APIs
-app
-  .route("/")
-  .get((req, res) => {
-    res.render("home.ejs");
-  })
-  .post()
-  .put()
-  .patch()
-  .delete();
-
-app.get("/form", (req, res) => {
-  res.render("form.ejs");
+app.get("/", (req, res) => {
+  res.render("home.ejs");
 });
 
 app
@@ -129,20 +118,118 @@ app
   .get((req, res) => {
     res.render("register.ejs");
   })
-  .post()
-  .put()
-  .patch()
-  .delete();
+  .post((req, res) => {
+    const newUser = new UserModel({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    newUser.save();
+    res.redirect("/");
+  });
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
 app
-  .route("/login")
+  .route("/form")
   .get((req, res) => {
-    res.render("login.ejs");
+    res.render("form.ejs");
   })
-  .post()
-  .put()
-  .patch()
-  .delete();
+  .post((req, res) => {
+    //view all form inputs
+    console.log(req.body);
+
+    //assign form inputs to variables nee3ded for totalScore calculation
+    const fragrance = parseInt(req.body.fragranceFinalScore);
+    const aroma = parseInt(req.body.aromaFinalScore);
+    const flavor = parseInt(req.body.flavorFinalScore);
+    const aftertaste = parseInt(req.body.aftertasteFinalScore);
+    const acidity = parseInt(req.body.acidityFinalScore);
+    const sweetness = parseInt(req.body.sweetnessFinalScore);
+    const mouthfeel = parseInt(req.body.mouthfeelFinalScore);
+    const overall = parseInt(req.body.overallFinalScore);
+
+    // //Code to handle nonuniform = undefined in case of uniform cups
+    let nonuniform;
+    if (!req.body.nonuniformCup) {
+      nonuniform = 0;
+    } else {
+      nonuniform = req.body.nonuniformCup.length;
+    }
+    // //code to handle defects = undefined in case of no defective cups
+    let defects;
+    if (!req.body.defectCup) {
+      defects = 0;
+    } else {
+      defects = req.body.defectCup.length;
+    }
+
+    // // assign cupScore function value to totalScore variable to push to DB
+    const totalScore = cupScore(
+      fragrance,
+      aroma,
+      flavor,
+      aftertaste,
+      acidity,
+      sweetness,
+      mouthfeel,
+      overall,
+      nonuniform,
+      defects
+    );
+
+    console.log(totalScore);
+
+    const newScore = new ScoreModel({
+      cupperName: req.body.cupperName,
+      cuppingDate: req.body.cuppingDate,
+      purpose: req.body.purpose,
+      coffeeId: req.body.coffeeID,
+      fragranceIntensity: parseInt(req.body.fragranceIntensity),
+      fragranceScore: req.body.fragranceScore,
+      fragranceFinalScore: parseInt(req.body.fragranceFinalScore),
+      aromaIntensity: parseInt(req.body.aromaIntensity),
+      aromaScore: req.body.aromaScore,
+      aromaFinalScore: parseInt(req.body.aromaFinalScore),
+      smellCharacter: req.body.smellCharacter,
+      smellNotes: req.body.smellNotes,
+      flavorIntensity: parseInt(req.body.flavorIntensity),
+      flavorScore: req.body.flavorScore,
+      flavorFinalScore: parseInt(req.body.flavorFinalScore),
+      aftertasteIntensity: parseInt(req.body.aftertasteIntensity),
+      aftertasteScore: req.body.aftertasteScore,
+      aftertasteFinalScore: parseInt(req.body.aftertasteFinalScore),
+      flavorCharacter: req.body.flavorCharacter,
+      taste: req.body.taste,
+      tasteNotes: req.body.tasteNotes,
+      acidIntensity: parseInt(req.body.acidIntensity),
+      acidityScore: req.body.acidityScore,
+      acidityFinalScore: parseInt(req.body.acidityFinalScore),
+      acidityCharacter: req.body.acidityCharacter,
+      acidityNotes: req.body.acidityNotes,
+      sweetnessIntensity: parseInt(req.body.sweetnessIntensity),
+      sweetnessScore: req.body.sweetnessScore,
+      sweetnessFinalScore: parseInt(req.body.sweetnessFinalScore),
+      sweetnessNotes: req.body.sweetnessNotes,
+      mouthfeelIntensity: parseInt(req.body.mouthfeelIntensity),
+      mouthfeelScore: req.body.mouthfeelScore,
+      mouthfeelFinalScore: parseInt(req.body.mouthfeelFinalScore),
+      mouthfeelCharacter: req.body.mouthfeelCharacter,
+      mouthfeelNotes: req.body.mouthfeelNotes,
+      extrinsicNotes: req.body.extrinsicNotes,
+      overallScore: req.body.overallScore,
+      overallFinalScore: parseInt(req.body.overallFinalScore),
+      nonUniformCups: req.body.nonUniformCup,
+      defectiveCups: req.body.defectCup,
+      defectType: req.body.defectType,
+      totalScore: totalScore,
+    });
+
+    newScore.save();
+    //res.redirect("/dashboard");
+  });
 
 app.route("/coffee").get().post().put().patch().delete();
 app
@@ -160,8 +247,10 @@ app.post("/data", async (req, res) => {
 });
 
 app.post("/score", async (req, res) => {
+  //view all form inputs
   console.log(req.body);
 
+  //assign form inputs to variables nee3ded for totalScore calculation
   const fragrance = parseInt(req.body.fragranceFinalScore);
   const aroma = parseInt(req.body.aromaFinalScore);
   const flavor = parseInt(req.body.flavorFinalScore);
@@ -171,28 +260,44 @@ app.post("/score", async (req, res) => {
   const mouthfeel = parseInt(req.body.mouthfeelFinalScore);
   const overall = parseInt(req.body.overallFinalScore);
 
+  //Code to handle nonuniform = undefined in case of uniform cups
   let nonuniform;
   if (!req.body.nonuniformCup) {
     nonuniform = 0;
   } else {
     nonuniform = req.body.nonuniformCup.length;
   }
-
+  //code to handle defects = undefined in case of no defective cups
   let defects;
   if (!req.body.defectCup) {
     defects = 0;
   } else {
     defects = req.body.defectCup.length;
   }
-  console.log(
-    cupScore(fragrance, aroma, flavor, aftertaste, acidity, sweetness, mouthfeel, overall, nonuniform, defects)
+
+  // assign cupScore function value to totalScore variable to push to DB
+  const totalScore = cupScore(
+    fragrance,
+    aroma,
+    flavor,
+    aftertaste,
+    acidity,
+    sweetness,
+    mouthfeel,
+    overall,
+    nonuniform,
+    defects
   );
+
+  console.log(totalScore);
 });
 
+//Define the port for the app to listen on
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
 });
 
+//Function to calculate the cup score on the "form.ejs" page after submitting
 function cupScore(fragrance, aroma, flavor, aftertaste, acidity, sweetness, mouthfeel, overall, nonuniform, defects) {
   const numbers = [fragrance, aroma, flavor, aftertaste, acidity, sweetness, mouthfeel, overall];
 
